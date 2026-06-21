8478c6487941d273ac70910a4903528cfcfceccb
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-06-21 16:54:56 +0530

    Added Terraform security group module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,8 +1,13 @@
 module "vpc"{
     source = "../../modules/vpc"
-
     vpc_cidr = var.vpc_cidr
     availability_zone = var.availability_zone
     public_subnet_cidr = var.public_subnet_cidr
+    environment = var.environment
+}
+
+module "security_group" {
+    source = "../../modules/security-group"
+    vpc_id = module.vpc.vpc_id
     environment = var.environment
 }
--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "public_subnet_id" {
   value = module.vpc.public_subnet_id
 }
+
+output "security_group_id" {
+  value = module.security_group.security_group_id
+}
